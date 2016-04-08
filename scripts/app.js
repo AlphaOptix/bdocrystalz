abcb8ee5ae72abbcd4af6e9887aed66ec364a820
Brian Wilhite <Brian_Wilhite@intuit.com>
2016-04-07 17:38:57 -0700

    More Updates

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,13 @@
  * Created by bwilhite on 4/7/16.
  */
 angular
-    .module('crystalApp',[])
-    .controller('crystalCtrl',function(){
+    .module('crystalApp',['ngSanitize'])
+    .controller('crystalCtrl',function($sce){
         var vm = this;
 
-        vm.crystals = window.BDO.crystals;
+        var crystals = window.BDO.crystals;
+        for(var i = 0; i < crystals.length; i++){
+            crystals[i].ItemEffect = $sce.trustAsHtml(crystals[i].ItemEffect.replace(/\s\&\s/g, '<br />'));
+        }
+        vm.crystals = crystals;
     });
