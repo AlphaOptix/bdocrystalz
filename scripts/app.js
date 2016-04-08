390966198f71c355c0900896cdf062062a83d512
Brian Wilhite <Brian_Wilhite@intuit.com>
2016-04-07 21:55:49 -0700

    Fixing some sorting/filtering. Any Slot now shows up for all slots (but costume).

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,45 @@
  */
 angular
     .module('crystalApp',['ngSanitize'])
-    .controller('crystalCtrl',function($sce){
+    .controller('crystalCtrl',function(){
         var vm = this;
 
         var crystals = window.BDO.crystals;
         for(var i = 0; i < crystals.length; i++){
-            crystals[i].ItemEffect = $sce.trustAsHtml(crystals[i].ItemEffect.replace(/\s\&\s/g, '<br />'));
+            crystals[i].ItemEffect = crystals[i].ItemEffect.replace(/\s\&\s/g, '<br />');
         }
         vm.crystals = crystals;
-    });
+        vm.search = {};
+    })
+    .filter('crystal', function(){
+        return function(input, compares) {
+            var out = input;
+
+            if (compares.Slot&&compares.Slot != "") {
+                out = out.filter(function (value) {
+                    return (value.Slot == "Any" && compares.Slot != "Costume") || value.Slot == compares.Slot;
+                });
+            }
+
+            if (compares.Rarity&&compares.Rarity != "") {
+                out = out.filter(function (value) {
+                    return value.Rarity == compares.Rarity;
+                });
+            }
+
+            if (compares.BreakChance&&compares.BreakChance != "") {
+                out = out.filter(function (value) {
+                    return value.BreakChance == compares.BreakChance;
+                });
+            }
+
+            if (compares.ItemEffect&&compares.ItemEffect != "") {
+                out = out.filter(function (value) {
+                    return value.ItemEffect.toLowerCase().indexOf(compares.ItemEffect.toLowerCase()) != -1;
+                });
+            }
+
+            return out;
+        }
+    })
+    .filter('unsafe', function($sce) { return $sce.trustAsHtml; });
