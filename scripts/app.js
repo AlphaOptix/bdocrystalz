3e219aff89bfca10c968eabdca46bd3f69a0f215
Brian Wilhite <Brian_Wilhite@intuit.com>
2016-04-07 23:50:58 -0700

    Refactor a bit

--- scripts/app.js
+++ scripts/app.js
@@ -6,21 +6,21 @@ angular
         var crystals = window.BDO.crystals;
         vm.effects = [];
         for(var i = 0; i < crystals.length; i++){
-            var itemEffects = crystals[i].ItemEffect.split(' & ');
-            for(var j = 0; j < itemEffects.length; j++){
-                var plusIndex = itemEffects[j].indexOf('+');
-                var effect = itemEffects[j].substr(0,plusIndex).trim();
+            var effects = crystals[i].effect.split(' & ');
+            for(var j = 0; j < effects.length; j++){
+                var plusIndex = effects[j].indexOf('+');
+                var effect = effects[j].substr(0,plusIndex).trim();
                 if(effect!=""&&vm.effects.map(function(e){return e.name}).indexOf(effect)==-1){
                     vm.effects.push({name:effect, value:effect});
                 }
             }
-            crystals[i].ItemEffect = crystals[i].ItemEffect.replace(/\s\&\s/g, '<br />');
+            crystals[i].effect = crystals[i].effect.replace(/\s\&\s/g, '<br />');
         }
         vm.effects.sort(sortByName);
         vm.effects.unshift({name:"All", value:""});
         vm.crystals = crystals;
         vm.search = {};
-        vm.search.ItemEffect = "";
+        vm.search.effect = "";
 
         function sortByName(a,b){
             if (a.name < b.name)
@@ -35,30 +35,30 @@ angular
         return function(input, compares) {
             var out = input;
 
-            if (compares.Slot&&compares.Slot != "") {
+            if (compares.socket&&compares.socket != "") {
                 out = out.filter(function (value) {
-                    return (value.Slot == "Any" && compares.Slot != "Costume") || value.Slot == compares.Slot;
+                    return (value.socket == "Any" && compares.socket != "Costume") || value.socket == compares.socket;
                 });
             }
 
-            if (compares.Rarity&&compares.Rarity != "") {
+            if (compares.grade&&compares.grade != "") {
                 out = out.filter(function (value) {
-                    return value.Rarity == compares.Rarity;
+                    return value.grade == compares.grade;
                 });
             }
 
-            if (compares.BreakChance&&compares.BreakChance != "") {
+            if (compares.breakChance&&compares.breakChance != "") {
                 out = out.filter(function (value) {
-                    return value.BreakChance == compares.BreakChance;
+                    return value.breakChance == compares.breakChance;
                 });
             }
 
-            if (compares.ItemEffect&&compares.ItemEffect != "") {
+            if (compares.effect&&compares.effect != "") {
                 out = out.filter(function (value) {
-                    var effects = value.ItemEffect.split('<br />');
+                    var effects = value.effect.split('<br />');
                     var isGood = false;
                     for(var i = 0;i<effects.length; i++){
-                        if(effects[i].toLowerCase().indexOf(compares.ItemEffect.toLowerCase() + " +") == 0){
+                        if(effects[i].toLowerCase().indexOf(compares.effect.toLowerCase() + " +") == 0){
                             isGood = true;
                         }
                     }
