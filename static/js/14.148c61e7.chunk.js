b3ef81bd79f02020888e698dfe9f46be6902af98
dynisor <dynisor@gmail.com>
2021-05-13 05:51:26 +0000

    Deploying to gh-pages from @ AlphaOptix/bdocrystalz@b52013fb618db53addc4d8119aea1333dcecd8f2 🚀

--- static/js/14.148c61e7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbdocrystalz-react"]=this["webpackJsonpbdocrystalz-react"]||[]).push([[14],{162:function(e,a,t){"use strict";t.r(a);var r=t(74),c=t(159),s=t(170),n=t(160),h=t(2);a.default=function(e){var a=e.searchValue,t=e.setSearchValue;return Object(h.jsx)(r.a,{children:Object(h.jsxs)(c.a,{style:{width:"100%"},children:[Object(h.jsx)(s.a,{htmlFor:"search",shrink:!0,children:"Name"}),Object(h.jsx)(n.a,{placeholder:"",id:"search",value:a,onChange:function(e){return t(e.target.value)}})]})})}}}]);
-//# sourceMappingURL=14.148c61e7.chunk.js.map
